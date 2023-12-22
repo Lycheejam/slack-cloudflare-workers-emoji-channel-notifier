ac1d585febfc649647db6479cdce29a97cbc2aae
lycheejam <lycheejamwow@gmail.com>
2023-12-23 03:03:19 +0900

    とりあえず動いた

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,57 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler deploy src/index.ts --name my-worker` to deploy your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { SlackApp, SlackEdgeAppEnv } from "slack-cloudflare-workers";
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
+export interface Env extends SlackEdgeAppEnv {
+  POST_CHANNEL_ID_EMOJI: string;
+  POST_CHANNEL_ID_CHANNEL: string;
 }
 
 export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		return new Response("Hello World!");
-	},
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    const slackApp = new SlackApp({ env })
+      .event("emoji_changed", async ({ context, payload }) => {
+        if (payload.subtype === "add") {
+          await context.client.chat.postMessage({
+            channel: env.POST_CHANNEL_ID_EMOJI,
+            text: `:tada: 新しいemojiが追加されました : \`:${payload.name}:\` :${payload.name}:`,
+          });
+        }
+      })
+      
+      .event("channel_created", async ({ context, payload }) => {
+        await context.client.chat.postMessage({
+          channel: env.POST_CHANNEL_ID_CHANNEL,
+          text: `:tada: チャンネル #${payload.channel.name} が作成されました！`,
+          link_names: true,
+        });
+      })
+
+      .event("channel_rename", async ({ context, payload }) => {
+        await context.client.chat.postMessage({
+          channel: env.POST_CHANNEL_ID_CHANNEL,
+          text: `:pencil2: チャンネル #${payload.channel.name} の名前が変更されたようです。`,
+          link_names: true,
+        });
+      })
+
+      .event("channel_archive", async ({ context, payload }) => {
+        await context.client.chat.postMessage({
+          channel: env.POST_CHANNEL_ID_CHANNEL,
+          text: `:notebook: チャンネル ${payload.channel} がアーカイブされたようです。`,
+          link_names: true,
+        });
+      })
+
+      .event("channel_deleted", async ({ context, payload }) => {
+        await context.client.chat.postMessage({
+          channel: env.POST_CHANNEL_ID_CHANNEL,
+          text: `:shower: チャンネル ${payload.channel} が削除されたようです。`,
+          link_names: true,
+        });
+      })
+    return await slackApp.run(request, ctx);
+  },
 };
